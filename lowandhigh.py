504e87746a42253c6161e509df6aa1eb34998975
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2022-12-20 11:41:10 +0300

    Create lowandhigh.py

---
+++ lowandhigh.py
@@ -0,0 +1,21 @@
+#Examples
+
+#high_and_low("1 2 3 4 5")  # return "5 1"
+#high_and_low("1 2 -3 4 5") # return "5 -3"
+#high_and_low("1 9 3 4 -5") # return "9 -5"
+
+
+# create the function
+def lowandhigh(string):
+
+    # We are going to remove the white space in the string
+    nospacestring = string.split()
+
+    # Put each element in a list
+    list = []       # empty list
+    for i in nospacestring:
+        list.append(int(i))
+
+    # print the result
+    return str(max(list))+' '+str(min(list))
+    
