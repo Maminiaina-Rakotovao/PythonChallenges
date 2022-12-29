2ce17ac8a8f78e69dcca688ec7d5b752dc742dce
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2022-12-29 17:10:05 +0300

    Update pyramid_array.py

--- pyramid_array.py
+++ pyramid_array.py
@@ -1,3 +1,12 @@
+#Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+#pyramid(0) => [ ]
+#pyramid(1) => [ [1] ]
+#pyramid(2) => [ [1], [1, 1] ]
+#pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+
+
+
 def pyramid(number):
     mydict= dict()
     for i in range(1,number+1):
