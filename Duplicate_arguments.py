5b97e0622b1ff2c8de4ea44018bfc0aec0c43a23
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2023-01-04 12:25:04 +0300

    Add files via upload

---
+++ Duplicate_arguments.py
@@ -0,0 +1,15 @@
+# Instructions
+
+# Complete the solution so that it returns true if it contains any duplicate argument values. Any number of arguments may be passed into the function.
+
+# The array values passed in will only be strings or numbers. The only valid return values are true and false.
+
+# Examples:
+
+# solution(1, 2, 3)             -->  false
+# solution(1, 2, 3, 2)          -->  true
+# solution('1', '2', '3', '2')  -->  true
+
+
+def solution(*args):
+    return True if [x for x in args if args.count(x) > 1] else False
