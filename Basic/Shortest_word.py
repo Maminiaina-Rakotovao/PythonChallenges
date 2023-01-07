24d4f6443ff346d5656fc0c47e00977f2624eddc
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2023-01-07 12:08:54 +0300

    Update and rename Shortest_word.py to Basic/Shortest_word.py

--- Basic/Shortest_word.py
+++ Basic/Shortest_word.py
@@ -8,10 +8,5 @@ def shortest_word(s):
     s_list = s.split()
     for i in s_list:
         my_list.append(len(i))
-    print(min(my_list))
+    return min(my_list)
 
-shortest_word("bitcoin take over the world maybe who knows perhaps")
-
-# correction
-# def find_short(s):
-#     return min(len(x) for x in s.split())
