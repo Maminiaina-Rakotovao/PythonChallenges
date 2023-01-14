411bca7eb109eb24aa9c1aa7512aedbed5e94800
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2023-01-14 09:59:36 +0300

    Update Backward_string.py

--- Basic/Backward_string.py
+++ Basic/Backward_string.py
@@ -3,17 +3,17 @@
 # Ex: Good --> dooG
 
 
-# Create a void list to store the text
-backward_list = []
+
+
 
 # Create the backward function
 def backward(text):
-   # j = 1
+   
+   # Create a void list to store the text
+   backward_list = []
+   
     for i in range(1,len(text)+1):
         backward_list.append(text[len(text)-i])
-       # j = j + 1
-    print(''.join(backward_list))
+       
+    return ''.join(backward_list)
 
-n = input('Enter: ')
-
-backward(n)
