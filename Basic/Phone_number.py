da72bc4e4391bdca60194863f3a2fdf2b4c4dccc
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2023-01-14 09:54:47 +0300

    Update Phone_number.py

--- Basic/Phone_number.py
+++ Basic/Phone_number.py
@@ -7,7 +7,5 @@ def Phone_number(n):
     string = ''.join(str(i) for i in n)
     p1 = '('+string[:3]+')'
     p2 = string[3:6]+'-'+string[6:]
-    #print(p1,p2)
-    print(p1+' '+p2)
-
-Phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+   
+    return p1+' '+p2
