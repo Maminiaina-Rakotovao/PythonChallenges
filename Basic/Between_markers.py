80acb54ffb2117a530243579ae948ac14b6b5e88
Maminiaina-Rakotovao <95483687+Maminiaina-Rakotovao@users.noreply.github.com>
2023-01-14 09:52:55 +0300

    Update Between_markers.py

--- Basic/Between_markers.py
+++ Basic/Between_markers.py
@@ -1,6 +1,9 @@
 # This program will find a substring between an enclosed two markers
 
+# Example: What is [apple]','[','] ==> apple
+
 # Create the function
+
 def Between_markers(text,marker1,marker2):
     # find the index of the marker1 and marker2
     index_marker1 = text.index(marker1)
@@ -8,6 +11,4 @@ def Between_markers(text,marker1,marker2):
 
     #display the result
     result = text[index_marker1+1:index_marker2]
-    print(result)
-
-Between_markers('What is [apple]','[',']')
+    return result
