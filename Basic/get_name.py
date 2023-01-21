18e2126b561c849d31a5859befbf2e11376f92c4
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-01-21 21:20:44 +0300

    get names

---
+++ Basic/get_name.py
@@ -0,0 +1,18 @@
+# The following code is not giving the expected results. Can you debug what the issue is?
+
+# The following is an example of data that would be passed in to the function.
+
+# data = [
+#     {'name': 'Joe', 'age': 20},
+#     {'name': 'Bill', 'age': 30},
+#     {'name': 'Kate', 'age': 23}
+# ]
+# get_names(data) # should return ['Joe', 'Bill', 'Kate']
+
+
+
+def get_names(data):
+    result = []
+    for i in range(len(data)):
+        result.append(data[i]['name'])
+    return result
