9344de198e0ef86a175b935ee5ec52afdb139721
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-02-15 20:47:06 +0300

    matrix-creation

---
+++ Basic/matrix-creation.py
@@ -0,0 +1,20 @@
+# Create an identity matrix of the specified size( >= 0).
+
+# Some examples:
+
+# (1)  =>  [[1]]
+
+# (2) => [ [1,0],
+#         [0,1] ]
+
+#       [ [1,0,0,0,0],
+#         [0,1,0,0,0],
+# (5) =>   [0,0,1,0,0],
+#         [0,0,0,1,0],
+#         [0,0,0,0,1] ]   
+
+import numpy as np
+def get_matrix(n):
+    #your code here
+    return np.eye(n, dtype=int).tolist()
+
