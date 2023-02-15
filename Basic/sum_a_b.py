0496d5df9f38f009fbd52ff900d996c3a8b2639a
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-02-15 13:46:22 +0300

    sum

--- Basic/sum_a_b.py
+++ Basic/sum_a_b.py
@@ -8,11 +8,9 @@ def get_sum(a,b):
         while b <= a:
             s = s + b
             b += 1
-    print(s)
+    return s
 
 
-n1 = input('Enter the first number: ')
-n1 = int(n1)
-n2 = input('Enter the first number: ')
-n2 = int(n2)
+n1 = int(input('Enter the first number: '))
+n2 = int(input('Enter the first number: '))
 get_sum(n1,n2)
