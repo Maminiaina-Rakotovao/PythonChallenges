01b270ace62d1c5a53b20bf936990782accbe61d
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-03-03 20:29:36 +0300

    Pyramid array

--- Basic/pyramid_array.py
+++ Basic/pyramid_array.py
@@ -1,10 +1,13 @@
-#Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+"""
 
-#pyramid(0) => [ ]
-#pyramid(1) => [ [1] ]
-#pyramid(2) => [ [1], [1, 1] ]
-#pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
 
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+
+"""
 
 
 def pyramid(number):
