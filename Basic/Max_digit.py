310a8e779c323c87fe8959c5c31145f887ecd492
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-03-03 20:21:16 +0300

    Max digit

--- Basic/Max_digit.py
+++ Basic/Max_digit.py
@@ -1,8 +1,14 @@
-# This program will determine which digit in the given number is the biggest
-# Ex: input --> output
-# 1 --> 1
-# 251 --> 5
-# 74 --> 7
+"""
+
+
+This program will determine which digit in the given number is the biggest
+Ex: input --> output
+1 --> 1
+251 --> 5
+74 --> 7
+
+
+"""
 
 # Create a list to store variable
 digit_list = []
