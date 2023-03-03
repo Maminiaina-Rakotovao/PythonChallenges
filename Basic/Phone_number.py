8329e1f72425b7e462ef282926eb3919378f7367
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-03-03 20:26:54 +0300

    Phone number

--- Basic/Phone_number.py
+++ Basic/Phone_number.py
@@ -1,7 +1,11 @@
-# Ex: create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+"""
+
+Ex: create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
+returns "(123) 456-7890"
 
 
-# Create the function
+"""
+Create the function
 def Phone_number(n):
    
     string = ''.join(str(i) for i in n)
