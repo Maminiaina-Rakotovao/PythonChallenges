be584f3f98d00cf32e90a1b01abb2d0aa7d28210
Maminiaina Rakotovao <maminiainarakotovao0599@gmail.com>
2023-03-03 20:28:23 +0300

    matrix creation

--- Basic/matrix-creation.py
+++ Basic/matrix-creation.py
@@ -1,18 +1,21 @@
-# Create an identity matrix of the specified size( >= 0).
+"""
 
-# Some examples:
+Create an identity matrix of the specified size( >= 0).
 
-# (1)  =>  [[1]]
+Some examples:
+(1)  =>  [[1]]
 
-# (2) => [ [1,0],
-#         [0,1] ]
+(2) => [ [1,0],
+        [0,1] ]
 
-#       [ [1,0,0,0,0],
-#         [0,1,0,0,0],
-# (5) =>   [0,0,1,0,0],
-#         [0,0,0,1,0],
-#         [0,0,0,0,1] ]   
+	      [[1,0,0,0,0],
+           [0,1,0,0,0],
+(5) =>     [0,0,1,0,0],
+           [0,0,0,1,0],
+           [0,0,0,0,1]]   
 
+
+"""
 import numpy as np
 def get_matrix(n):
     #your code here
